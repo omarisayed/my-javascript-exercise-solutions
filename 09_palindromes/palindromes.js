efab4ea4677cb3e996912e4bc3e07542afd8bba3
omarisayed <osayedunoff@gmail.com>
2023-03-12 09:08:39 +0300

    Corrected mistakes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,13 @@
 const palindromes = function (str) {
-    str = str.replace(/\s+/g, ''); //first, remove all the spaces from the string.
-    str = str.replace(/\,/g, ''); //remove all commas.
+    str = str.replace(/[^A-Za-z0-9]/g, '');   //  removes all non-alphanumeric characters from a string, including spaces.
     str = str.toLowerCase(); //convert whole string to lower case.
-    str = str.split(''); //then, split the string into an array of single characters
     
-    strArray = str;
-    strReversed = str.reverse(strArray);
+    str_2 = str.split('').reverse().join('');
     
-    if (strArray === strReversed){
-        return true;
-    }else{
-        return false;
+    if (str === str_2){
+        return true
     }
+    return false;
 };
 
 // Do not edit below this line
@@ -21,6 +17,13 @@ module.exports = palindromes;
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// I first removed all the spaces from a string. e.g. 'race car' becomes 'racecar'
-// Then I split it into single characters i.e. ['r','a','c','e','c','a','r']
-// A palindrome needs to be same forwards and backwards. So, I check if the array is the samewith the original when it is reversed. If it is, return true, i.e. it is a palindrome.
+
+//A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+// You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+//
+//
+//
+// I first removed all non-alphanumeric characters
+//Then I converted the whole string to lowercase e.g., --> 'Acaramanamaraca' --> becomes --> 'acaramanamaraca'
+// Then,in another variabale, I split the string into single characters, reversed it, then joined it to form a string again.
+// If the strings match in original and reversed format, then it is palindrome. Else, it is not.
