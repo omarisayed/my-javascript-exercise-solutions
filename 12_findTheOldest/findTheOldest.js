bab3c4796ad715c6e6c098bef323f365be8db5e0
omarisayed <osayedunoff@gmail.com>
2023-03-13 07:16:58 +0300

    Added comments.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -28,9 +28,8 @@ module.exports = findTheOldest;
 
 
 
-
-
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 // if the person is still living, get todays date and assign it to death. because we will count until today which is the last day anyways.
 // This is same as having a max variable that holds the maximum value and updating it when the current value is larger than maximum value.
+// We use reduce() because sort() would arrange the whole array in ascending and descending order and return the whole array. But the code should return one output i.e., the oldest person.
